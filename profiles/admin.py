458f96086d09d495395aa9a4b8561113399e7f9f
codelover1110 <code.lower1110@gmx.com>
2024-03-04 15:27:06 -0800

    showed phone number type field

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'surname', 'email', 'phone_number', 'phone_number_type']
+
+# Register the UserProfile model with the custom admin class
+admin.site.register(UserProfile, UserProfileAdmin)
